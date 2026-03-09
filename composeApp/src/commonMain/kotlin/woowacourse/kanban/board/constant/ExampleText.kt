b31fd943b61d4839998ddb2fd63569e61331076b
yws <dbdntjs620@gmail.com>
2026-03-09 13:07:30 +0900

    refactor : MyConst.kt  관심사에 따른 분리

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/constant/ExampleText.kt
@@ -0,0 +1,8 @@
+package woowacourse.kanban.board.constant
+
+const val EXAMPLE_TITLE = "LazyColumn 컴포넌트 구현"
+const val EXAMPLE_CONTENT = "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다."
+const val EXAMPLE_WRITER = "다이노"
+const val MAX_TITLE = "너무너무 긴 제목은 한 줄까지만 노출합니다."
+const val MAX_CONTENT = "너무너무너무 긴 설명은 두 줄까지만 노출하고 말줄임표로 처리합니다 두 줄까지만 노출합니다."
+const val MAX_WRITER = "너무너무너무 긴 담당자도 한 줄까지만 노출합니다."
